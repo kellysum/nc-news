f892792822fa514df9c7b7f54ea1a6bc3070416b
Kellyshek <sheksumyin@gmail.com>
2023-12-12 16:15:22 +0000

    change author to username in CommentCard

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -2,7 +2,7 @@ const CommentCard = ({ comment }) => {
     return (
         <div className="comment-card">
             <p className="comment-body">{comment.body}</p>
-            <p>Author: {comment.author}</p>
+            <p>Username: {comment.author}</p>
             <p>Created at:{comment.created_at}</p>
             
         </div>
